e8fc463c4db9ce245d0161d59d475d3542b942e5
melisacar <acarme17@itu.edu.tr>
2025-02-04 11:05:03 +0300

    feat: see product-card infos

--- main-api.py
+++ main-api.py
@@ -1,19 +1,18 @@
 import requests
 
-# API'nin adresi
-api_url = "https://api.hakmarexpress.com.tr/api/home/slug/ev-yasam-c"
+# Ev ve yasam category
+url = "https://api.hakmarexpress.com.tr/api/home/slug/ev-yasam-c"
 
-# Headers ekleyerek tarayıcı gibi görünmesini sağlıyoruz
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
+response = requests.get(url)
+data = response.json()
 
-# API'ye GET isteği gönder
-response = requests.get(api_url, headers=headers)
+products = data["page"][1]["columns"][1]["contents"][1]["columns"][0]["content"]["products"]
 
-# Başarılı yanıt aldıysak
-if response.status_code == 200:
-    data = response.json()  # JSON formatına çevir
-    print(data)  # Tüm veriyi görmek için
-else:
-    print("API'ye erişilemedi!")
+for product in products:
+    product_info = product.get("product", {})
+    print(f"ID: {product_info.get('id')}")
+    print(f"Name: {product_info.get('name')}")
+    print(f"Price: {product_info.get('price')} TL")
+    print(f"Image URL: {product_info.get('imageUrl')}")
+    print("-" * 50)
+
